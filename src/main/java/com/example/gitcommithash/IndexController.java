e358798832350b002ab4bbbf2231404f312438bd
Yusuke Yamamoto <yusuke@mac.com>
2022-04-03 22:40:28 +0900

    initial commit

---
+++ src/main/java/com/example/gitcommithash/IndexController.java
@@ -0,0 +1,16 @@
+package com.example.gitcommithash;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    String index(){
+        return "index";
+    }
+    public static String getImplementationVersion() {
+        return IndexController.class.getPackage().getImplementationVersion();
+    }
+
+}
